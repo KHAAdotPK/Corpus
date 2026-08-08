55679b646ee7523a37662b8703e14c3bf14aa261
KHAAdotPK <spysee.pk@gmail.com>
2026-08-08 18:23:38 +0500

    Update Corpus header with MAX_VOCAB_SIZE and Serialisation include
    - Add MAX_VOCAB_SIZE macro (undef first to avoid redefinition warnings)
    - Include Serialisation.hh for serialisation support
    - Improve top-level comment describing header purpose and add maintainer info

--- header.hh
+++ header.hh
@@ -1,12 +1,20 @@
 /*
     lib/Corpus/header.hh
-    Q@hackers.pk
+
+    This header file serves as a central inclusion point for all necessary tables created by Parser and provides a unfied interface to access elements to those tables
+    
+    Maintainer: Sohail.
  */
 
 #ifndef CSV_CORPUS_LIB_CORPUS_HEADER_HH
 #define CSV_CORPUS_LIB_CORPUS_HEADER_HH
 
 #include <fstream>
+
+#ifdef MAX_VOCAB_SIZE
+#undef MAX_VOCAB_SIZE
+#endif
+#define MAX_VOCAB_SIZE 20000
 
 #include "./../Parser/header.hh"
 
